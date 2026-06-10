e8f0725c59384cf2966d79e9a8d2ae831d597e6c
solomon-solo2552 <solomonsolo2552@gmail.com>
2026-06-10 10:16:27 +0530

    Day 2: Product model and admin panel setup

---
+++ backend/shop/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
